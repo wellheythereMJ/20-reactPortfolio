4a21730887b9d48d65cb22c1d2095fb39f9edd90
wellheythereMJ <abetterplace@outlook.com>
2024-05-24 14:03:12 -0700

    installed starting code and created folder, updated app.jsx, main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <Outlet />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './pages/about.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      }
+    ]
+  }])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
