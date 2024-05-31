9482a30d068904a136e78edbcab07a002e2f1f99
wellheythereMJ <abetterplace@outlook.com>
2024-05-31 13:09:46 -0700

    contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,22 @@
 export default function Contact() {
   return (
-    <div><h1>Contact</h1></div>
+    <div className="container"><h1>Contact</h1>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Name</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" required/>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
