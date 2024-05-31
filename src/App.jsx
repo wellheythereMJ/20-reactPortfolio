2bc485828bc7f4435371db5cad38209d8459ddd2
wellheythereMJ <abetterplace@outlook.com>
2024-05-31 12:35:06 -0700

    app.jsx, header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet } from 'react-router-dom'
+import Header from './components/header.jsx'
 
 function App() {
 
   return (
     <>
+    <Header />
     <Outlet />
     </>
   )
