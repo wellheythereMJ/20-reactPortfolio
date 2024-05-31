139b26d72dfb3a98bd659bb4cf7697d26e12d861
wellheythereMJ <abetterplace@outlook.com>
2024-05-31 13:28:48 -0700

    github icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Outlet } from 'react-router-dom'
-import Header from './components/header.jsx'
+import { Outlet } from 'react-router-dom';
+import Header from './components/header.jsx';
+import Footer from './components/footer.jsx';
+
 
 function App() {
 
   return (
     <>
     <Header />
+    <main className='min-vh-100'>
     <Outlet />
+    </main>
+    <Footer />
     </>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer (){
+  return (
+    <footer className="footer sticky-bottom">
+      <div className="container">
+        <span className="text-muted">Copyright &copy; 2024</span>
+        <a href="https://github.com/wellheythereMJ" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-github"></i></a>
+      </div>
+    </footer>
+  )
+}
